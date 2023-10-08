b032b4019362fdb97c44367bd837685ea4db170a
Cameron Dodd <cadodd@ucsc.edu>
2023-10-08 15:22:08 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.type = "button";
+button.textContent = "👻";
+
+app.append(button);
